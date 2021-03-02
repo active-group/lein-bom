b83701f59daab17e31d00502dd7847e7bb493f75
Marco Schneider <marco.schneider@active-group.de>
2021-03-02 14:13:34 +0100

    Add url to github repository.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject de.active-group/lein-bom "0.1.0-SNAPSHOT"
   :description "Generate a BOM file from a project.clj."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/active-group/lein-bom"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[clj-commons/pomegranate "1.2.0"]
